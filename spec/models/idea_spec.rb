f6cf2b978a2b8959ea747ca2e372ceadb8985813
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-15 15:26:09 -0600

    create idea model with validations and enums for quality * added test as well; for some reason, test for enums doesn't pass when there is a validation for inclusion of quality in [0, 1, 2]

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Idea, type: :model do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:quality) }
+  it { should respond_to(:quality?)}
+
+  it "properly maps enums to quality types" do
+    idea1 = create(:idea, quality: 0)
+    idea2 = create(:idea, quality: 1)
+    idea3 = create(:idea, quality: 2)
+
+    expect(idea1.quality).to eq("swill")
+    expect(idea2.quality).to eq("plausible")
+    expect(idea3.quality).to eq("genius")
+  end
+end
