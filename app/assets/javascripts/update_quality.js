8e1681d88b074b305aeb96bda457b1ebd2637b86
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-17 17:16:35 -0600

    add functionality to up vote or down vote ideas

---
+++ app/assets/javascripts/update_quality.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+  function improveQuality(currentQuality){
+    // console.log(currentQuality)
+    if (currentQuality == "swill") {
+      currentQuality = "plausible"
+    } else {
+      currentQuality = "genius"
+    }
+    return currentQuality
+    // console.log(currentQuality)
+  }
+
+  function downgradeQuality(currentQuality){
+    if (currentQuality == "genius") {
+      currentQuality = "plausible"
+    } else {
+      currentQuality = "swill"
+    }
+    return currentQuality
+  }
+
+  $(".ideas-list").on("click", ".upvote", function(){
+    var currentQuality = $(this).siblings(".quality").text();
+    var ideaId = $(this).parent().data("id")
+    var updatedQuality = improveQuality(currentQuality)
+    $.ajax({
+      url: "/api/v1/ideas/" + ideaId,
+      method: "PATCH",
+      dataType: "JSON",
+      data: { idea: { quality: updatedQuality}},
+      success: function(idea){
+        $("#quality-" + idea.id).text(updatedQuality)
+      },
+      error: errorMessage,
+    })
+  })
+
+  $(".ideas-list").on("click", ".downvote", function(){
+    var currentQuality = $(this).siblings(".quality").text();
+    var ideaId = $(this).parent().data("id")
+    var updatedQuality = downgradeQuality(currentQuality)
+    $.ajax({
+      url: "/api/v1/ideas/" + ideaId,
+      method: "PATCH",
+      dataType: "JSON",
+      data: { idea: { quality: updatedQuality}},
+      success: function(idea){
+        $("#quality-" + idea.id).text(updatedQuality)
+      },
+      error: errorMessage,
+    })
+  })
+});
