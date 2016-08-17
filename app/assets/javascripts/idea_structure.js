1a6b1d0888d2e6b6af18af51401c6723bd0f3ce1
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-17 17:16:16 -0600

    tweak html structure of ideas div

--- app/assets/javascripts/idea_structure.js
+++ app/assets/javascripts/idea_structure.js
@@ -1,14 +1,15 @@
 function ideaStructure(idea){
   return "<div class='idea-full' data-id=" + idea.id
             + "><div class='idea-body'>"
-              + "<div class='title' id='title-'" + idea.id
-              + " contenteditable='true'>" + idea.title + "</div>"
-              + "<div class='body' id='body-'" + idea.id
-              + " contenteditable='true'>" + idea.body + "</div>"
-            + "<div class='idea-features'>"
-              + idea.quality
-              + "<button class='upvote btn btn-default'>Upvote</button>"
-              + "<button class='downvote btn btn-default'>Downvote</button>"
+              + "<div class='title' id='title-" + idea.id
+              + "' contenteditable='true'>" + idea.title + "</div>"
+              + "<div class='body' id='body-" + idea.id
+              + "' contenteditable='true'>" + idea.body + "</div>"
+            + "<div class='idea-features' data-id=" + idea.id + ">"
+              + "<div class='quality' id='quality-" + idea.id + "'>"
+              + idea.quality + "</div>"
+              + "<button class='upvote   btn btn-default' id='upvote-" + idea.id + "'>Upvote</button>"
+              + "<button class='downvote btn btn-default' id='downvote-" + idea.id + "'>Downvote</button>"
               + "<button data-id=" + idea.id
               + " class='btn btn-warning delete-idea' type='button'>Delete</button>"
             + "</div>"
